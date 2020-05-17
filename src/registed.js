0db0a57ac04eaf26095cc0bebf6634f359ff923f
1184457686 <1184457686@qq.com>
2020-05-17 16:06:52 +0800

    first-one

--- src/registed.js
+++ src/registed.js
@@ -10,50 +10,49 @@ import {
 } from 'react-native'
 import { NavigationContainer, StackActions } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-import {Request} from '../api/Request';
+import Request from '../api/Request';
 import { Value } from 'react-native-reanimated';
 var width = Dimensions.get('screen').width;
 var height = Dimensions.get('screen').height;
 export default class registe extends Component {
-    state={
-        email:'',
-        password:''
+    state = {
+        email: '',
+        password: ''
     }
-    RegisteAccount=()=>{
-        const data={
-            'account_id':this.state.email,
-            'password':this.state.password
+    RegisteAccount = () => {
+        const data = {
+            'account_id': this.state.email,
+            'password': this.state.password
         }
 
-        Request('/1/account',data,'POST')
-        .then(res=>{
-            if(res.ok){
-                Alert.alert('注册成功！')
-                navigation.navigator('登录')
-            }else{
-                const error =res.error_type==undefined?'邮箱格式错误':res.message
-                Alert.alert(error)
-            }
-        })
+        Request('/1/account', data, 'POST')
+            .then(res => {
+                if (res.ok) {
+                    Alert.alert('注册成功！')
+                    navigation.navigator('登录')
+                } else {
+                    const error = res.error_type == undefined ? '邮箱格式错误' : res.message
+                    Alert.alert(error)
+                }
+            })
     }
-    checkInput=()=>{
+    checkInput = () => {
         var pattern = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-        const {email,password}=this.state;
-        // if(email==''||password==''){
-        //     Alert.alert("邮箱或密码不能为空");
-        // }else if(!email.match(pattern)){
-        //     Alert.alert("邮箱格式不正确")
-        // }else if(password.match()){
-        // }
-        // else{
-        //     this.RegisteAccount();
-        // }
-        this.RegisteAccount();
+        const { email, password } = this.state;
+        if (email == '' || password == '') {
+            Alert.alert("邮箱或密码不能为空");
+        } else if (!email.match(pattern) || password.match()) {
+            Alert.alert("邮箱格式不正确")
+        }
+        else {
+            this.RegisteAccount();
+        }
+
     }
 
     render() {
         const { navigation } = this.props;
-        const {email,password} = this.state;
+        const { email, password } = this.state;
         return (
             <View style={{ backgroundColor: 'Silver', width: width, height: height, justifyContent: 'center' }}>
                 <View style={styles.loginView}>
@@ -63,19 +62,19 @@ export default class registe extends Component {
                             placeholder={'请输入邮箱'}
                             visible-password={email}
                             style={styles.loginText}
-                            onChangeText={value=>{
-                                this.setState={
-                                    email:value
+                            onChangeText={value => {
+                                this.setState = {
+                                    email: value
                                 }
                             }}
-                             />
+                        />
                         <TextInput
                             placeholder={'请输入密码'}
                             style={styles.loginText}
                             secureTextEntry={true}
-                            onChangeText={(value)=>{
-                                this.setState={
-                                    password:value
+                            onChangeText={(value) => {
+                                this.setState = {
+                                    password: value
                                 }
                             }}
                         />
@@ -84,15 +83,15 @@ export default class registe extends Component {
                         activeOpacity={0.1}
                         style={styles.loginBthStyle}
                         // onPress={()=>this.checkInput()}
-                        onPress={()=>this.checkInput()}
+                        onPress={() => this.checkInput()}
                     >
                         <Text>注册</Text>
 
                     </TouchableOpacity>
                     <TouchableOpacity
                         activeOpacity={0.1}
-                        onPress={()=>navigation.navigate('登录')}
-                        
+                        onPress={() => navigation.navigate('登录')}
+
                     >
                         <Text>已有账号?返回登录</Text>
                     </TouchableOpacity>
